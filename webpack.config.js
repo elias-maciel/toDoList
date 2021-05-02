4b0a0a388a73504acf15d1cc62a02a21f282c840
elias-maciel <eliasfsilvamaciel@gmail.com>
2021-05-02 19:17:08 -0300

    animações css

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+
+module.exports ={
+    entry: ['@babel/polyfill', path.resolve(__dirname, './src/main.js')],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    }
+}
