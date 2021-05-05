24e1c25a2bfbb4002d99b7ca73bcdc0d47bba620
elias-maciel <eliasfsilvamaciel@gmail.com>
2021-05-05 12:14:49 -0300

    ToDoList criada

---
+++ src/main.js
@@ -0,0 +1,49 @@
+import { createApp } from 'vue'
+import Todos from './api/todos'
+import './assets/css/style.css'
+
+const apiTodos = new Todos()
+
+const app = createApp({
+data(){
+    return{
+        todos: [],
+        form: {
+            text: '',
+            done: false
+        }
+    }
+},
+created(){
+    this.fetchTodos()
+},
+methods:{
+    async fetchTodos(){
+        this.todos = await apiTodos.index()
+    },
+    async createTodo(){
+        const data = await apiTodos.store(this.form)
+        this.todos.push(data)
+
+        this.form.text= ''
+        this.form.done = false
+    },
+    async toggleTodoStatus(todo){
+        const data = await apiTodos.update({
+            ...todo,
+            done: !todo.done,
+        })
+
+        const index = this.todos.findIndex(({id}) => id == data.id)
+        this.todos[index] = data
+    },
+    async destroyTodo(id){
+        await apiTodos.destroy({id})
+        const index = this.todos.findIndex((todo) => todo.id == id)
+        this.todos.splice(index, 1)
+    }
+}
+})
+
+app.mount('#app')
+
